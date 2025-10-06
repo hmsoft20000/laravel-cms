f0effa55b9fde29a5fbb11f287aeb08ceef0de05
hmsoft20000 <hmsoft2000@gmail.com>
2025-10-06 12:05:55 +0300

    Update constant.php
    check define  before  define variables

--- src/Utils/constant.php
+++ src/Utils/constant.php
@@ -1,104 +1,42 @@
 <?php
 
-const DEFAULT_DATA_LIMIT = 20;
-const DEFAULT_PAGE = 1;
-
-const SECTOR_IMAGE_NAME = "sector";
-const USERS_IMAGE_NAME = "users";
-const ORGANIZATION_IMAGE_NAME = "organizations";
-
-const BLOG_IMAGE_NAME = "blog";
-const BLOG_ATTRIBUTE_IMAGE_NAME = "blogsAttributes";
-const BLOG_FEATURE_IMAGE_NAME = "blogsFeatures";
-
-const SERVICE_IMAGE_NAME = "service";
-const SERVICE_ATTRIBUTE_IMAGE_NAME = "servicesAttributes";
-const SERVICE_FEATURE_IMAGE_NAME = "servicesFeatures";
-
-
-
-
-
-
-
-
-
-
-
-
-const DEFAULT_IMAGE_NAME = "def.png";
-const SLIDER_IMAGE_NAME = "main-slider";
-const EMPTY_HTML_CONTENT = "<p></p>";
-
-const IMAGE_FORMATE = "webp";
-const DEFAULT_STRING = "";
-
-const IMAGE_NAME = "";
-const PORTFOLIO_IMAGE_NAME = "portfolio";
-const PORTFOLIO_ATTRIBUTE_IMAGE_NAME = "portfoliosAttributes";
-const PORTFOLIO_FEATURE_IMAGE_NAME = "portfoliosFeatures";
-const PORTFOLIO_DOWNLOAD_FILE_NAME = "downloads/portfolios";
-
-
-
-
-const ITEMS_REVIEW_IMAGE_NAME = "items_reviews";
-const FEATURE_IMAGE_NAME = "features";
-const STATISTICS_IMAGE_NAME = "statistics";
-const REWADS_IMAGE_NAME = "rewads";
-const ABOUT_IMAGE_NAME = "aboutUs";
-const PRIVACY_POLICY_IMAGE_NAME = "privacyPolicy";
-const TERM_OF_SERVICE_IMAGE_NAME = "termOfService";
-const TERM_OF_USE_IMAGE_NAME = "termOfUse";
-const TERM_AND_CONDITION_IMAGE_NAME = "termAndCondition";
-const REFUND_POLICY_IMAGE_NAME = "refundPolicy";
-const OUR_MISSION_IMAGE_NAME = "ourMission";
-const OUR_STORY_IMAGE_NAME = "ourStory";
-const OUR_HISTORY_IMAGE_NAME = "ourHistory";
-const OUR_VALUES_IMAGE_NAME = "ourValues";
-const OUR_VISION_IMAGE_NAME = "ourVision";
-const BRAND_IMAGE_NAME = "brands";
-const DEFAULTS_IMAGE_NAME = "defaults";
-const TESTIMONIAL_IMAGE = "testimonial";
-
-const TEAMS_IMAGE_NAME = "teams";
-
-
-const owlProperties = [
-    'center' => false,
-    'rtl' => true,
-    'items' => 2.1,
-    'loop' => true,
-    'autoplay' => true,
-    'autoplayHoverPause' => true,
-    'margin' => 5,
-    'nav' => false,
-    'dots' => false,
-    'responsive' => [
-        // xs
-        '0' => [
-            'items' => 1.2,
-            // "margin" => 0
-        ],
-        // sm
-        '576' => [
-            'items' => 2.2,
-        ],
-        // md
-        '768' => [
-            'items' => 2.2,
-        ],
-        // lg
-        '992' => [
-            'items' => 3.2,
-        ],
-        // xl
-        '1200' => [
-            'items' => 4.2,
-        ],
-        // xxl
-        '1400' => [
-            'items' => 4.6,
-        ],
-    ],
-];
+if (!defined('DEFAULT_DATA_LIMIT')) define('DEFAULT_DATA_LIMIT', 20);
+if (!defined('DEFAULT_PAGE')) define('DEFAULT_PAGE', 1);
+if (!defined('SECTOR_IMAGE_NAME')) define('SECTOR_IMAGE_NAME', "sector");
+if (!defined('USERS_IMAGE_NAME')) define('USERS_IMAGE_NAME', "users");
+if (!defined('ORGANIZATION_IMAGE_NAME')) define('ORGANIZATION_IMAGE_NAME', "organizations");
+if (!defined('BLOG_IMAGE_NAME')) define('BLOG_IMAGE_NAME', "blog");
+if (!defined('BLOG_ATTRIBUTE_IMAGE_NAME')) define('BLOG_ATTRIBUTE_IMAGE_NAME', "blogsAttributes");
+if (!defined('BLOG_FEATURE_IMAGE_NAME')) define('BLOG_FEATURE_IMAGE_NAME', "blogsFeatures");
+if (!defined('SERVICE_IMAGE_NAME')) define('SERVICE_IMAGE_NAME', "service");
+if (!defined('SERVICE_ATTRIBUTE_IMAGE_NAME')) define('SERVICE_ATTRIBUTE_IMAGE_NAME', "servicesAttributes");
+if (!defined('SERVICE_FEATURE_IMAGE_NAME')) define('SERVICE_FEATURE_IMAGE_NAME', "servicesFeatures");
+if (!defined('DEFAULT_IMAGE_NAME')) define('DEFAULT_IMAGE_NAME', "def.png");
+if (!defined('SLIDER_IMAGE_NAME')) define('SLIDER_IMAGE_NAME', "main-slider");
+if (!defined('EMPTY_HTML_CONTENT')) define('EMPTY_HTML_CONTENT', "<p></p>");
+if (!defined('IMAGE_FORMATE')) define('IMAGE_FORMATE', "webp");
+if (!defined('DEFAULT_STRING')) define('DEFAULT_STRING', "");
+if (!defined('IMAGE_NAME')) define('IMAGE_NAME', "");
+if (!defined('PORTFOLIO_IMAGE_NAME')) define('PORTFOLIO_IMAGE_NAME', "portfolio");
+if (!defined('PORTFOLIO_ATTRIBUTE_IMAGE_NAME')) define('PORTFOLIO_ATTRIBUTE_IMAGE_NAME', "portfoliosAttributes");
+if (!defined('PORTFOLIO_FEATURE_IMAGE_NAME')) define('PORTFOLIO_FEATURE_IMAGE_NAME', "portfoliosFeatures");
+if (!defined('PORTFOLIO_DOWNLOAD_FILE_NAME')) define('PORTFOLIO_DOWNLOAD_FILE_NAME', "downloads/portfolios");
+if (!defined('ITEMS_REVIEW_IMAGE_NAME')) define('ITEMS_REVIEW_IMAGE_NAME', "items_reviews");
+if (!defined('FEATURE_IMAGE_NAME')) define('FEATURE_IMAGE_NAME', "features");
+if (!defined('STATISTICS_IMAGE_NAME')) define('STATISTICS_IMAGE_NAME', "statistics");
+if (!defined('REWADS_IMAGE_NAME')) define('REWADS_IMAGE_NAME', "rewads");
+if (!defined('ABOUT_IMAGE_NAME')) define('ABOUT_IMAGE_NAME', "aboutUs");
+if (!defined('PRIVACY_POLICY_IMAGE_NAME')) define('PRIVACY_POLICY_IMAGE_NAME', "privacyPolicy");
+if (!defined('TERM_OF_SERVICE_IMAGE_NAME')) define('TERM_OF_SERVICE_IMAGE_NAME', "termOfService");
+if (!defined('TERM_OF_USE_IMAGE_NAME')) define('TERM_OF_USE_IMAGE_NAME', "termOfUse");
+if (!defined('TERM_AND_CONDITION_IMAGE_NAME')) define('TERM_AND_CONDITION_IMAGE_NAME', "termAndCondition");
+if (!defined('REFUND_POLICY_IMAGE_NAME')) define('REFUND_POLICY_IMAGE_NAME', "refundPolicy");
+if (!defined('OUR_MISSION_IMAGE_NAME')) define('OUR_MISSION_IMAGE_NAME', "ourMission");
+if (!defined('OUR_STORY_IMAGE_NAME')) define('OUR_STORY_IMAGE_NAME', "ourStory");
+if (!defined('OUR_HISTORY_IMAGE_NAME')) define('OUR_HISTORY_IMAGE_NAME', "ourHistory");
+if (!defined('OUR_VALUES_IMAGE_NAME')) define('OUR_VALUES_IMAGE_NAME', "ourValues");
+if (!defined('OUR_VISION_IMAGE_NAME')) define('OUR_VISION_IMAGE_NAME', "ourVision");
+if (!defined('BRAND_IMAGE_NAME')) define('BRAND_IMAGE_NAME', "brands");
+if (!defined('DEFAULTS_IMAGE_NAME')) define('DEFAULTS_IMAGE_NAME', "defaults");
+if (!defined('TESTIMONIAL_IMAGE')) define('TESTIMONIAL_IMAGE', "testimonial");
+if (!defined('TEAMS_IMAGE_NAME')) define('TEAMS_IMAGE_NAME', "teams");
